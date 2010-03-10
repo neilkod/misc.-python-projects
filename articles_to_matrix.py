889df6e6e81e35c482e633cbf840a23774c57a08
Neil Kodner <nkodner@gmail.com>
2010-03-10 06:33:37 -0700

    tweaked word limiting, added k-means clustering

--- articles_to_matrix.py
+++ articles_to_matrix.py
@@ -1,7 +1,82 @@
 #!/usr/bin/python
 import re
 import codecs
+from math import sqrt
 
+def pearson(v1,v2):
+	# simple sums
+	sum1 = sum(v1)
+	sum2 = sum(v2)
+	
+	# sums of the squares
+	sum1Sq = sum([pow(v,2) for v in v1])
+	sum2Sq = sum([pow(v,2) for v in v2])
+	
+	# sum of the products
+	pSum = sum([v1[i] * v2[i] for i in range(len(v1))])
+	
+	# Calculate r (Pearson score)
+	num = pSum - (sum1 * sum2 / len(v1))
+	den=sqrt((sum1Sq - pow(sum1,2) / len(v1)) * (sum2Sq-pow(sum2,2)/len(v1)))
+	if den ==0: return 0
+	
+	return 1.0 - num/den
+	
+	
+def kcluster(rows,distance=pearson,k=4):
+""" taken directly from the programming collective intelligence book"""
+  # Determine the minimum and maximum values for each point
+  ranges=[(min([row[i] for row in rows]),max([row[i] for row in rows])) 
+  for i in range(len(rows[0]))]
+
+  # Create k randomly placed centroids
+  clusters=[[random.random()*(ranges[i][1]-ranges[i][0])+ranges[i][0] 
+  for i in range(len(rows[0]))] for j in range(k)]
+  
+  lastmatches=None
+  for t in range(100):
+    print 'Iteration %d' % t
+    bestmatches=[[] for i in range(k)]
+    
+    # Find which centroid is the closest for each row
+    for j in range(len(rows)):
+      row=rows[j]
+      bestmatch=0
+      for i in range(k):
+        d=distance(clusters[i],row)
+        if d<distance(clusters[bestmatch],row): bestmatch=i
+      bestmatches[bestmatch].append(j)
+
+    # If the results are the same as last time, this is complete
+    if bestmatches==lastmatches: break
+    lastmatches=bestmatches
+    
+    # Move the centroids to the average of their members
+    for i in range(k):
+      avgs=[0.0]*len(rows[0])
+      if len(bestmatches[i])>0:
+        for rowid in bestmatches[i]:
+          for m in range(len(rows[rowid])):
+            avgs[m]+=rows[rowid][m]
+        for j in range(len(avgs)):
+          avgs[j]/=len(bestmatches[i])
+        clusters[i]=avgs
+      
+  return bestmatches	
+  
+def readfile(filename):
+	lines=[line for line in file(filename)]	
+	# first line is the column titles
+	colnames = lines[0].strip().split('\t')[1:]
+	rownames=[]
+	data=[]
+	for line in lines[1:]:
+		p=line.strip().split('\t')
+		# first column in each row is rowname
+		rownames.append(p[0])
+		# the data for this row is the remainder of the row
+		data.append([float(x) for x in p[1:]])
+	return rownames, colnames,data
 
 def countwords(txt):
 	""" return a dictionary containing the word-frequencies in txt"""
@@ -43,8 +118,11 @@ def buildWordlist(articles):
 	
 def buildWordlist2(articles):
 	wordlist = []
-	stopwords=['a','the','but','of','to','for','who','and','by','our','as','be','in','is','must','are','this','hr','if','i']
+	stopwords = ['a', 'am', 'and', 'are', 'as', 'be', 'but', 'by', 'for', 'hr', 'i', 'if', 'in', 'is', 'must', 'of', 'our', 'the', 'this', 'to', 'who']
 	print 'article length is %s' %len(articles)
+	
+	# I had trouble splitting out the following comprehension so its ugly.  big to-do is to clean
+	# this up using comprehensions and not for loops
 	for val in articles.values():
 		[wordlist.append(wd) for wd in val.keys() if wd not in wordlist and wd not in stopwords and float(apcount[wd])/len(articles)>.04]
 	wordlist.sort()
@@ -88,14 +166,6 @@ if len(wordlist) <50:
 	#print articles
 
 
-#take a random word and test the frequency
-
-
-print len(wordlist)
-
-# so now, we have all of the article ids and we have all of the words
-# lets build a matrix
+	# write the matrix itself
 writeMatrix('neilwords.txt',articles,wordlist)
 
-print wordlist
-	
